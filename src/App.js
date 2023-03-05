f70b7d074ec8ad7d30ff24cb0fd6d6954089c30f
sm8sk4l <sebastianmoskal@protonmail.com>
2023-03-05 15:36:28 +0100

    image gallery almost done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 import "./App.css";
 
 function App() {
-  return <div className="text-4xl">Hej</div>;
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => {
+            return <ImageCard key={image.id} image={image} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
